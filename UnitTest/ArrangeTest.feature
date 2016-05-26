ab8cd8156973f25154510eed339d865e1d31dfd3
Icer <liangshuangde@163.com>
2016-05-27 06:31:41 +0800

    enhance the unittest

---
+++ UnitTest/ArrangeTest.feature
@@ -0,0 +1,15 @@
+﻿Feature: ArrangeTest
+	In order to avoid silly mistakes
+	As a user
+	I want to be told the result of smart arrange.
+
+Scenario: Standard example
+	Given I have following files to be arranged
+	| Filename                     |
+	| E:\aa\sp1                    |
+	| E:\aa\sp-23412.231.txt       |
+	| E:\aa\sp-23412.231(1).txt    |
+	| E:\aa\sp-23412.231(1)(1).cmd |
+	| E:\aa\sp2                    |
+	When I start smart arrange
+	Then the result should be `E:\aa\sp`
---
+++ UnitTest/ArrangeTestSteps.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTest
+{
+    using System;
+    using System.Linq;
+    using IcerSystem.FileArrange;
+    using NUnit.Framework;
+    using TechTalk.SpecFlow;
+    using TechTalk.SpecFlow.Assist;
+
+    [Binding]
+    public class ArrangeTestSteps
+    {
+        [Given(@"I have following files to be arranged")]
+        public static void GivenIHaveFollowingFilesToBeArranged(Table table)
+        {
+            ScenarioContext.Current.Set(table, "FileTable");
+        }
+
+        [When(@"I start smart arrange")]
+        public static void WhenIStartSmartArrange()
+        {
+            var table = ScenarioContext.Current.Get<Table>("FileTable");
+            var files = table.Rows.Select(_ => _["Filename"]).ToArray();
+            var output = CommonStringExtension.GetCommonSubstring(files);
+            ScenarioContext.Current.Set(output, "ArrangeResult");
+
+        }
+
+        [Then(@"the result should be `(.*)`")]
+        public static void ThenTheResultShouldBe(string path)
+        {
+            var output = ScenarioContext.Current.Get<string>("ArrangeResult");
+            Assert.AreEqual(path, output);
+        }
+
+    }
+}
